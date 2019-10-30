c522dc409086cfa2282046de26b4373adaa91450
ikroskun <merle.liukun@gmail.com>
2019-10-30 22:13:47 +0800

    feat(ch1-3):

--- src/gopl.v1/ch1/ch1_3/dup1/dup1.go
+++ src/gopl.v1/ch1/ch1_3/dup1/dup1.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	counts := make(map[string]int)  // int is default 0
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		counts[input.Text()] ++
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
+
+// go run dup1.go
+/*
+Input:
+ikaros
+ikaros
+ik
+ik
+Output:  use CTRL+D (EOF)
+2	ikaros
+2	ik
+*/
--- src/gopl.v1/ch1/ch1_3/ex4/dup2_filename.go
+++ src/gopl.v1/ch1/ch1_3/ex4/dup2_filename.go
@@ -1,5 +1,52 @@
+// code for exercise 1.4
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	counts := make(map[string]int)
+
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
 }
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		text := input.Text()
+		counts[text] ++
+		if counts[text] > 1{
+			fmt.Printf("Text: %s, Repeat in %s\n", text, f.Name())
+		}
+	}
+}
+
+// go run dup2.go /path/to/file
+/*
+Output:
+Text: import, Repeat in /path/to/file
+2	lk
+*/
