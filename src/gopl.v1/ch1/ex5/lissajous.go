4689e47210944c9364ba52b1239083914c0345c5
ikroskun <merle.liukun@gmail.com>
2019-10-30 23:03:16 +0800

    feat(ch1-4):

---
+++ src/gopl.v1/ch1/ex5/lissajous.go
@@ -0,0 +1,58 @@
+// code for exercise 1.5
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.RGBA{0x0F, 0xDF, 0x0C, 0xFF}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
+
+// go build lissajous.go
+// ./lissajous > out.gif
---
+++ src/gopl.v1/ch1/ex6/lissajous.go
@@ -0,0 +1,64 @@
+// code for exercise 1.6
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.RGBA{0x0F, 0xDF, 0x0C, 0xFF}, color.RGBA{0x1C, 0x1E, 0xB3, 0xFF}}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+	greenIndex = 2
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+
+	for i, idx := 0, 1; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(idx))
+		}
+		// 遍历后几种颜色
+		idx++
+		if idx > len(palette) {
+			idx = 1
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
+
+// go build lissajous.go
+// ./lissajous > out.gif
